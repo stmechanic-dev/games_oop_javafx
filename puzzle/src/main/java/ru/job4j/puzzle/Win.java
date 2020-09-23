7a570d37b33aeb01db36c11654559cb7949c5c38
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-23 11:02:21 +0300

    2. Сокобан. Тестовое задание. [#358338]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,7 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = false;
+        boolean rsl = false; //result
         int rowCount = 0;
         int cellCount = 0;
         for (int i = 0; i < board.length; i++) {
